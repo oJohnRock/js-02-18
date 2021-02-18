ecbd7587adec2e2c59f58a30392e72bf4f797c3d
AlesejF <l7933@yandex.ru>
2021-02-19 01:21:23 +0300

    dz1_1

--- students/Алексей Федоров/project/script.js
+++ students/Алексей Федоров/project/script.js
@@ -7,13 +7,22 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
   
-const renderGoodsItem = (title, price) => `<div class="goods-item"><h3>${title}</h3><p>${price}$</p></div>`;
+const renderGoodsItem = (title, price) => `
+    <div class="goods-item">
+        <h3>${title}</h3>
+        <p>${price}$</p>
+        <button class="product-button" type="button">Добавить</button>
+    </div>`;
   
-const renderGoodsList = (list) => {
+const renderGoodsList = (list = goods) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
     
     document.querySelector('.goods-list').innerHTML = goodsList.join(' ');
+    
+    console.log(document.querySelector('.wrap_header'));
   
 }
   
-renderGoodsList(goods);
+renderGoodsList();
+
+//<div class="cart-button-counter"><p>0</p></div>
