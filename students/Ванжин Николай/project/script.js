5fee47de0d4a15d577c0d3bcb47918b3b45967cf
Nikolay <nikolay.vanzhin@yandex.ru>
2021-02-22 13:21:27 +0500

    create img, update style.css

--- students/Ванжин Николай/project/script.js
+++ students/Ванжин Николай/project/script.js
@@ -12,22 +12,22 @@ const goods = [{
 	},
 	{
 		title: 'Ноутбук',
-		price: 35000
+		price: 35000,
+		picture: `img/notebook.jpg`
 	},
 ];
 
-const getGoodsLayout = (title, price) => {
-	return `
-        <div class="item">
+const getGoodsLayout = (title, price, picture = `img/NOFOTO.jpg`) => `
+		<div class="item">
+		<img src="${picture}" alt="${title}">
             <h2>${title}</h2>
             <p>${price}</p>
         </div>
     `;
-}
 
 const renderGoods = (list) => {
-	const goodsString = list.map(element =>
-		getGoodsLayout(element.title, element.price)).join("");
+	const goodsString = list.map(element => //запятая выводилась т.к. метод map создает новый массив,а элементы массива разделены запятой по умолчанию
+		getGoodsLayout(element.title, element.price, element.picture)).join("");
 	document.querySelector('.goods').innerHTML = goodsString;
 }
 
