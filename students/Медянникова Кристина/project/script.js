5b562fd35e4a51dda38cfc658d38a2dc4b2029a5
KristinaMedyannikova <medyannikova90@mail.ru>
2021-02-28 22:04:11 +0300

    Initial commit

--- students/Медянникова Кристина/project/script.js
+++ students/Медянникова Кристина/project/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const goods = [
-  { title: 'Mango People T-shirt', price: 150 },
+/*const goods = [
+  { title: 'Mango People T-shirt', price: 200 },
   { title: 'Mango People T-shirt', price: 150 },
   { title: 'Mango People T-shirt', price: 150 },
 ];
@@ -16,17 +16,93 @@ const renderGoodsItem = (title, price) => {
         </div>`;
   };
   
-  const renderGoodsList = (list) => {
-      /*beforeend( сразу перед закрывающим тегом element (после последнего потомка))- параметр DOMString который определеет позицию добовляймого 
-      элемента относительно элемента, вызвавшего метод.
-      По умолчанию, элементы массива разделены запятой, это можно исправить передав в качестве параметра метода значение, 
-      которое будет использовано в качестве разделителя, а именно метод join(''), так как он объединяет все элементы массива 
-      (или массивоподобного объекта) в строку. */
-      
+  const renderGoodsList = (list) => {  
     document.querySelector('.goods-list').insertAdjacentHTML('beforeend', list.map(item => renderGoodsItem(item.title, item.price)).join(''));
 
-    /*let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;*/
 };
   
-  renderGoodsList(goods);
+  renderGoodsList(goods);*/
+
+//отрисовываем список товаров в HTML через класс
+ // класса GoodsItem и запрашивать его разметку
+  class GoodsItem {
+    constructor(item) {
+        this.item = item;
+    }
+
+    render() {
+        return `
+        <div class='goods-item'>
+            <h3 class='product-title'>${this.item.title}</h3> 
+            <p class='product-price'>${this.item.price} $</p>
+            <button class="by-btn">В корзину</button>
+        </div>
+        `;
+    }
+}
+
+ // Метод вывод списка товаров. Для каждого элемента массива goods будем создавать экземпляр   
+class GoodsList {
+    constructor(goodsArray) {
+        this.goods = goodsArray;
+    }
+
+    fetchData() {
+        this.goods = [
+          { title: 'Mango People T-shirt', price: 200 },
+          { title: 'Mango People T-shirt', price: 150 },
+          { title: 'Mango People T-shirt', price: 150 },
+        ];
+    }
+
+    render() {
+        const goodsString = this.goods.map(element => {
+            const item = new GoodsItem(element);
+            return item.render();
+        });
+        document.querySelector('.goods-list').innerHTML = goodsString.join('');
+    }
+}
+
+//создаем класс корзины Basket 
+class Basket {
+  constructor () {
+    this.goods = [];
+}
+    //метод добавления товара в корзину
+  addBasketItem(basketItem) {
+      this.goods.push(basketItem);
+   
+  }
+  deleteItem () {
+    //метод удаление из корзины товар 
+  }
+
+  fetchData() {
+     // метод запроса данных с сервера
+        
+    }
+  
+    render() {
+      //отрисовываем корзину через let в listHtml
+      let listHtml = '';
+     
+  
+    }
+    //Метод для вывода итоговой суммы всех товаров в корзине
+    totalBasketPrice() {
+      let totalPrice = document.getElementById('goods-list__total_price'); 
+      let sum = 0;
+      this.goods.forEach (goods => { 
+          sum += goods.price
+      });
+      totalPrice.innerText = `Итого  ${sum} $`;
+}
+
+
+
+
+const list = new GoodsList(this.goods);
+list.fetchData();
+list.render();
+list.totalCartPrice();
