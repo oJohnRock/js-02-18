e451b7f18de60bafcb98bf73569a0f309558e381
Europka <egorio1337@gmail.com>
2021-03-01 18:59:45 +0300

    * Доделал 1 и 2 задание

--- students/Егор Сенько/project/js1/script.js
+++ students/Егор Сенько/project/js1/script.js
@@ -7,7 +7,7 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
   
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title = 'Empty', price = 'Empty') => {
     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 };
   
@@ -15,5 +15,5 @@ const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
     document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
-  
+
 renderGoodsList(goods);
--- students/Егор Сенько/project/js2/script.js
+++ students/Егор Сенько/project/js2/script.js
@@ -1,26 +1,63 @@
-class Cart {
+"use strict";
+
+class GoodsList {
     constructor() {
+      this.goods = [];
     }
 
-    addProduct() {
+    fetchGoods() {
+      this.goods = [
+        { title: 'Shirt', price: 150 },
+        { title: 'Socks', price: 50 },
+        { title: 'Jacket', price: 350 },
+        { title: 'Shoes', price: 250 },
+      ];
+    }
+
+    render() {
+      let listHtml = '';
+      this.goods.forEach(good => {
+        const goodItem = new GoodsItem(good.title, good.price);
+        listHtml += goodItem.render();
+      });
+      document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+    /*addProduct() {
     }
 
     removeProduct() {
-    }
+    }*/
 
     totalPrice() {
+        let fullPrice = 0;
+
+        for (let i = 0; i < this.goods.length; i++) {
+            fullPrice += this.goods[i].price;
+        }
+
+        return fullPrice;
     }
 }
 
-class Hamburger {
-    constructor(size, stuffing) { ... }
-    addTopping(topping) {    // Добавить добавку }
-    removeTopping(topping) { // Убрать добавку }
-    getToppings(topping) {   // Получить список добавок }
-    getSize() {              // Узнать размер гамбургера }
-    getStuffing() {          // Узнать начинку гамбургера }
-    calculatePrice() {       // Узнать цену }
-    calculateCalories() {    // Узнать калорийность }
+const goods = [
+    { title: 'Shirt', price: 150 },
+    { title: 'Socks', price: 50 },
+    { title: 'Jacket', price: 350 },
+    { title: 'Shoes', price: 250 },
+];
+  
+const renderGoodsItem = (title = 'Empty', price = 'Empty') => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+  
+const renderGoodsList = (list) => {
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 
-let cheeseburger = new Hamburger()
+renderGoodsList(goods);
+
+let cart = new GoodsList();
+
+cart.fetchGoods();
