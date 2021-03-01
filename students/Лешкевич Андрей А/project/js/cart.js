5f7a3b92b3dce3bb6cf2bca5884e071cc2527294
MagicGTS <magicgts@gmail.com>
2021-03-01 22:46:23 +0300

    Brand Shop Continue
    Previous Brand project was imported.
    Cart Class was adopted for working with shop items which can have size\color\price and etc. variations.

--- students/Лешкевич Андрей А/project/js/cart.js
+++ students/Лешкевич Андрей А/project/js/cart.js
@@ -19,21 +19,28 @@ class CCart {
         }
         this.items = init;
         this.catalog = cat;
-        this.statUpdate();
+        this.updateStat();
         for (let html of Object.values(this.html)) {
             html.clear();
             html.listFillByItems();
         }
     }
-    statUpdate() {
+    parseData(_obj) {
+        let item = _obj.closest("[data-catalog-item][data-catalog-item-t]");
+        if (item) {
+            return { id: parseInt(item.getAttribute("data-catalog-item")), t: parseInt(item.getAttribute("data-catalog-item-t")) }
+        }
+        return undefined;
+    }
+    updateStat() {
         this.stat = {
             products: 0,
             count: 0,
             grand_total: 0
         };
         for (let item of Object.values(this.items)) {
-            this.stat.grand_total += this.catalog.find(obj => { return obj.id === item.id }).price * item.amount;
-            this.stat.count += item.amount;
+            this.stat.grand_total += this.catalog.find(obj => { return obj.id === item.id }).type[item.type].price * item.quantity;
+            this.stat.count += item.quantity;
             this.stat.products++;
         }
         for (let html of Object.values(this.html)) {
@@ -41,80 +48,89 @@ class CCart {
         }
         return this.stat;
     }
-
-    itemAdd(_id) {
-        if (!this.itemInc(_id)) {
+    addAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.addItem(item.id, item.t);
+        }
+        return false;
+    }
+    addItem(_id, _t = 0, _q = 1) {
+        if (!this.incItem(_id, _t)) {
             this.items.push({
                 "id": _id,
-                "amount": 1
+                "type": _t,
+                "quantity": _q
             });
-            this.statUpdate();
+            this.updateStat();
             for (let html of Object.values(this.html)) {
-                html.itemAdd(_id);
+                html.addItem(_id, _t, _q);
             }
         }
+        return true;
     }
 
-    itemInc(_id) {
-        if (this.items.find(obj => { return obj.id === _id })) {
-            this.items.find(obj => { return obj.id === _id }).amount++;
-            this.statUpdate();
-
+    incItem(_id, _t) {
+        let item = this.items.find(obj => { return (obj.id === _id && obj.type === _t) });
+        if (item) {
+            item.quantity++;
+            this.updateStat();
             for (let html of Object.values(this.html)) {
-                html.updateItem(_id);
+                html.updateItem(_id, _t, item.quantity);
             }
             return true;
         }
         return false;
     }
 
-    itemDec(_id) {
-        let item = this.items.find(obj => { return obj.id === _id });
+    decItem(_id, _t) {
+        let item = this.items.find(obj => { return (obj.id === _id && obj.type === _t) });
         if (item) {
-            if (1 < item.amount) {
-                item.amount--;
+            if (1 < item.quantity) {
+                item.quantity--;
                 for (let html of Object.values(this.html)) {
-                    html.updateItem(_id);
+                    html.updateItem(_id, _t, item.quantity);
                 }
             } else {
-                this.items.splice(this.items.findIndex(obj => obj.id === _id), 1)
+                this.items.splice(this.items.findIndex(obj => (obj.id === _id && obj.type === _t)), 1)
                 for (let html of Object.values(this.html)) {
-                    html.itemRemove(_id);
+                    html.removeItem(_id, _t);
                 }
             }
-            this.statUpdate();
+            this.updateStat();
             return true;
         }
         return false;
     }
 
-    itemRemove(_id) {
-        let item = this.items.find(obj => { return obj.id === _id });
+    removeItem(_id, _t) {
+        let item = this.items.find(obj => { return (obj.id === _id && obj.type === _t) });
         if (item) {
-            this.items.splice(this.items.findIndex(obj => obj.id === _id), 1)
+            this.items.splice(this.items.findIndex(obj => (obj.id === _id && obj.type === _t)), 1)
+            //this.items.splice(item, 1)
             for (let html of Object.values(this.html)) {
-                html.itemRemove(_id);
+                html.removeItem(_id, _t);
             }
-            this.statUpdate();
+            this.updateStat();
             return true;
         }
         return false;
     }
-    updateItem(_id, _val) {
-        _val = parseInt(_val);
+    updateItem(_id, _t, _val) {
         if (0 > _val) {
             _val = 0;
         }
-        this.items[_id].amount = _val;
+        this.items.find(obj => { return (obj.id === _id && obj.type === _t) }).quantity = _val;
 
-        this.statUpdate();
+        this.updateStat();
         for (let html of Object.values(this.html)) {
-            html.updateItem(_id);
+            html.updateItem(_id, _t, _val);
         }
+        return true;
     }
     clear() {
         this.items = {};
-        this.statUpdate();
+        this.updateStat();
         for (let html of Object.values(this.html)) {
             html.clear();
         }
@@ -124,6 +140,7 @@ class CCartSmallHTML {
     html = null;
     cart = {};
     list = null;
+    attrs = { item: "data-cartsmall-item", type: "data-cartsmall-item-t" }
     constructor(obj, cart) {
         if (null !== obj) {
             this.html = obj;
@@ -133,25 +150,32 @@ class CCartSmallHTML {
             return null;
         }
     }
+    parseData(_obj) {
+        let item = _obj.closest(`[${this.attrs.item}][${this.attrs.type}]`);
+        if (item) {
+            return { id: parseInt(item.getAttribute(this.attrs.item)), t: parseInt(item.getAttribute(this.attrs.type)) }
+        }
+        return undefined;
+    }
     itemGen(_obj) {
-        return `<li id="cartSmallItem-${_obj.id}" class="btn-cart__ddc-item">
+        return `<li ${this.attrs.item}="${_obj.id}" ${this.attrs.type}="${_obj.type}" class="btn-cart__ddc-item">
                         <div class="col"><a href="#"><img src="${_obj.img}" alt=""></a></div>
                         <div class="col">
                             <p>${_obj.name}</p>
                             <p>${startGen(_obj.star)}</p>
                             <div>
-                                <p class="price-count">${_obj.amount} <span class="x-txt">x</span> $${float2str(_obj.price)}</p>
-                                <p class="sub-total clear-fix"> = $${float2str(_obj.price * _obj.amount)}</p>
+                                <p class="price-count">${_obj.quantity} <span class="x-txt">x</span> $${float2str(_obj.price)}</p>
+                                <p class="sub-total clear-fix"> = $${float2str(_obj.price * _obj.quantity)}</p>
                             </div>
                         </div>
                         <div class="col">
-                            <a href="cart/itemInc/${_obj.id}" onclick="cart.itemInc(${_obj.id});return false;">
+                            <a href="cart/incItem/${_obj.id}/${_obj.type}" onclick="cart.html['small'].incAct(this);return false;">
                                 <i class="fas fa-plus-circle"></i>
                             </a>
-                            <a href="cart/itemRemove/${_obj.id}" onclick="cart.itemRemove(${_obj.id});return false;">
+                            <a href="cart/removeItem/${_obj.id}/${_obj.type}" onclick="cart.html['small'].removeAct(this);return false;">
                                 <i class="fa fa-times-circle"></i>
                             </a>
-                            <a href="cart/itemDec/${_obj.id}" onclick="cart.itemDec(${_obj.id});return false;">
+                            <a href="cart/decItem/${_obj.id}/${_obj.type}" onclick="cart.html['small'].decAct(this);return false;">
                                 <i class="fas fa-minus-circle"></i>
                             </a>
                         </div>
@@ -169,35 +193,72 @@ class CCartSmallHTML {
             this.html.getElementsByClassName("btn-cart__count")[0].style.display = "none";
         }
     }
-    itemAdd(_id) {
+    addAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.addItem(item.id, item.t);
+        }
+        return false;
+    }
+    decAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.decItem(item.id, item.t);
+        }
+        return false;
+    }
+    incAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.incItem(item.id, item.t);
+        }
+        return false;
+    }
+    addItem(_id, _t = 0, _q = 1) {
         const regex = /(\..+$)/gm;
         let item = this.cart.catalog.find(obj => { return obj.id === _id });
         this.list.insertAdjacentHTML('beforeend', this.itemGen({
             id: _id,
-            img: item.img.replace(regex, '_small.png'),
+            type: _t,
+            quantity: _q,
             name: item.name,
-            star: 4.5,
-            price: item.price,
-            amount: this.cart.items.find(obj => { return obj.id === _id }).amount
+            star: item.type[_t].star,
+            img: item.type[_t].img.replace(regex, '_small.png'),
+            price: item.type[_t].price
         }));
     }
-    itemRemove(_id) {
-        document.getElementById(`cartSmallItem-${_id}`).remove();
+    removeAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.removeItem(item.id, item.t);
+        }
+        return false;
+    }
+    removeItem(_id, _t = 0) {
+        this.html.querySelector(`[${this.attrs.item}='${_id}'][${this.attrs.type}='${_t}']`).remove();
     }
     listFillByItems() {
         for (let item of Object.values(this.cart.items)) {
-            this.itemAdd(item.id);
+            this.addItem(item.id, item.type, item.quantity);
         }
     }
     redraw(_obj) {
         this.clear();
         this.listFillByItems();
     }
-
-    updateItem(_id) {
-        let item = document.getElementById(`cartSmallItem-${_id}`);
-        item.getElementsByClassName("price-count")[0].innerHTML = `${this.cart.items.find(obj => { return obj.id === _id }).amount} <span class="x-txt">x</span> $${float2str(this.cart.catalog.find(obj => { return obj.id === _id }).price)}`;
-        item.getElementsByClassName("sub-total")[0].innerText = ` = $${float2str(this.cart.catalog.find(obj => { return obj.id === _id }).price * this.cart.items.find(obj => { return obj.id === _id }).amount)}`;
+    updateAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.updateItem(item.id, item.t, 1);
+        }
+        return false;
+    }
+    updateItem(_id, _t) {
+        let item = this.html.querySelector(`[${this.attrs.item}='${_id}'][${this.attrs.type}='${_t}']`);
+        let cart = this.cart.items.find(obj => { return (obj.id === _id && obj.type === _t) });
+        let price = this.cart.catalog.find(obj => { return obj.id === _id }).type[_t].price;
+        item.getElementsByClassName("price-count")[0].innerHTML = `${cart.quantity} <span class="x-txt">x</span> $${price}`;
+        item.getElementsByClassName("sub-total")[0].innerText = ` = $${float2str(price * cart.quantity)}`;
     }
 
 }
@@ -205,6 +266,7 @@ class CCartBigHTML {
     html = null;
     cart = {};
     list = null;
+    attrs = { item: "data-cartbig-item", type: "data-cartbig-item-t" }
     constructor(obj, cart) {
         if (null !== obj) {
             this.html = obj;
@@ -214,8 +276,15 @@ class CCartBigHTML {
             return null;
         }
     }
+    parseData(_obj) {
+        let item = _obj.closest(`[${this.attrs.item}][${this.attrs.type}]`);
+        if (item) {
+            return { id: parseInt(item.getAttribute(this.attrs.item)), t: parseInt(item.getAttribute(this.attrs.type)) }
+        }
+        return undefined;
+    }
     itemGen(_obj) {
-        return `      <div id="cartBigItem-${_obj.id}" class="product-item table-line">
+        return `      <div ${this.attrs.item}="${_obj.id}" ${this.attrs.type}="${_obj.type}" class="product-item table-line">
                             <div class="table-line__wrap">
                                 <div class="table-line-item table-line-item-prod">
                                     <div class="col"><a href="" class="item-img"><img src="${_obj.img}" alt=""></a></div>
@@ -227,10 +296,10 @@ class CCartBigHTML {
                                     </div>
                                 </div>
                                 <div class="table-line-item table-line-item-price">$${_obj.price}</div>
-                                <div class="table-line-item table-line-item-quant"><input class="item-quant" type="number" value="${_obj.amount}" onchange="cart.updateItem(${_obj.id},this.value);return false;" onblur="cart.updateItem(${_obj.id},this.value);return false;" min="0"></div>
+                                <div class="table-line-item table-line-item-quant"><input class="item-quant" type="number" value="${_obj.quantity}" onchange="cart.html['big'].updateAct(this);return false;" onblur="cart.html['big'].updateAct(this);return false;" min="0"></div>
                                 <div class="table-line-item table-line-item-ship">FREE</div>
-                                <div class="table-line-item table-line-item-subt">$${float2str(_obj.price * _obj.amount)}</div>
-                                <div class="table-line-item table-line-item-act"><a href="cart/itemRemove/${_obj.id}" onclick="cart.itemRemove(${_obj.id});return false;"><i class="fa fa-times-circle"></i></a></div>
+                                <div class="table-line-item table-line-item-subt">$${float2str(_obj.price * _obj.quantity)}</div>
+                                <div class="table-line-item table-line-item-act"><a href="cart/removeItem/${_obj.id}/${_obj.type}" onclick="cart.html['big'].removeAct(this);return false;"><i class="fa fa-times-circle"></i></a></div>
                             </div>
                         </div>`;
     }
@@ -242,37 +311,73 @@ class CCartBigHTML {
         cart_obj.getElementsByClassName("total-sub")[0].innerText = `$${float2str(this.cart.stat.grand_total)}`;
         cart_obj.getElementsByClassName("total-grand")[0].innerText = `$${float2str(this.cart.stat.grand_total)}`;
     }
-    itemAdd(_id) {
+    addAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.addItem(item.id, item.t);
+        }
+        return false;
+    }
+    decAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.decItem(item.id, item.t);
+        }
+        return false;
+    }
+    incAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.incItem(item.id, item.t);
+        }
+        return false;
+    }
+    addItem(_id, _t = 0, _q = 1) {
         const regex = /(\..+$)/gm;
         let item = this.cart.catalog.find(obj => { return obj.id === _id });
         this.list.insertAdjacentHTML('beforebegin', this.itemGen({
             id: _id,
-            img: item.img.replace(regex, '_small.png'),
+            type: _t,
+            quantity: _q,
             name: item.name,
-            star: 4.5,
-            price: item.price,
-            size: item.size,
-            color: item.color,
-            amount: this.cart.items.find(obj => { return obj.id === _id }).amount
+            star: item.type[_t].star,
+            img: item.type[_t].img.replace(regex, '_small.png'),
+            price: item.type[_t].price
         }));
     }
-    itemRemove(_id) {
-        document.getElementById(`cartBigItem-${_id}`).remove();
+    removeAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.removeItem(item.id, item.t);
+        }
+        return false;
+    }
+    removeItem(_id, _t = 0) {
+        this.html.querySelector(`[${this.attrs.item}='${_id}'][${this.attrs.type}='${_t}']`).remove();
     }
     listFillByItems() {
         for (let item of Object.values(this.cart.items)) {
-            this.itemAdd(item.id);
+            this.addItem(item.id, item.type, item.quantity);
         }
     }
     redraw(_obj) {
         this.clear();
         this.listFillByItems();
     }
+    updateAct(_obj) {
+        let item;
+        if ((item = this.parseData(_obj)) !== null) {
+            return this.cart.updateItem(item.id, item.t, parseInt(_obj.value));
+        }
+        return false;
+    }
+    updateItem(_id, _t) {
+        let item = this.html.querySelector(`[${this.attrs.item}='${_id}'][${this.attrs.type}='${_t}']`);
+        let cart = this.cart.items.find(obj => { return (obj.id === _id && obj.type === _t) });
+        let price = this.cart.catalog.find(obj => { return obj.id === _id }).type[_t].price;
 
-    updateItem(_id) {
-        let item = document.getElementById(`cartBigItem-${_id}`);
-        item.getElementsByClassName("item-quant")[0].value = this.cart.items.find(obj => { return obj.id === _id }).amount;
-        item.getElementsByClassName("table-line-item-subt")[0].innerText = `$${float2str(this.cart.catalog.find(obj => { return obj.id === _id }).price * this.cart.items.find(obj => { return obj.id === _id }).amount)}`;
+        item.getElementsByClassName("item-quant")[0].value = cart.quantity;
+        item.getElementsByClassName("table-line-item-subt")[0].innerText = `$${float2str(price * cart.quantity)}`;
     }
 
 }
--- students/Лешкевич Андрей А/project/js/catalog.js
+++ students/Лешкевич Андрей А/project/js/catalog.js
@@ -2,172 +2,235 @@
 var prodCatalog = [
     {
         id: 1,
-        img: "img/catalog_item_01.png",
         name: "Mango People T-shirt #1",
-        price: 52.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_01.png",
+            price: 52.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 2,
-        img: "img/catalog_item_02.png",
         name: "Mango People T-shirt #2",
-        price: 32.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_02.png",
+            price: 32.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 3,
-        img: "img/catalog_item_03.png",
         name: "Mango People T-shirt #3",
-        price: 42.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_03.png",
+            price: 42.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 4,
-        img: "img/catalog_item_04.png",
         name: "Mango People T-shirt #4",
-        price: 62.34,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_04.png",
+            price: 62.34,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 5,
-        img: "img/catalog_item_05.png",
         name: "Mango People T-shirt #5",
-        price: 54.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_05.png",
+            price: 54.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 6,
-        img: "img/catalog_item_06.png",
         name: "Mango People T-shirt #6",
-        price: 53.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_06.png",
+            price: 53.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 7,
-        img: "img/catalog_item_07.png",
         name: "Mango People T-shirt #7",
-        price: 58.60,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_07.png",
+            price: 58.60,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 8,
-        img: "img/catalog_item_08.png",
         name: "Mango People T-shirt #8",
-        price: 50.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_08.png",
+            price: 50.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
 
     {
         id: 9,
-        img: "img/catalog_item_09.png",
         name: "Mango People T-shirt #9",
-        price: 52.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_09.png",
+            price: 52.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 10,
-        img: "img/catalog_item_10.png",
         name: "Mango People T-shirt #10",
-        price: 32.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_10.png",
+            price: 32.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 11,
-        img: "img/catalog_item_11.png",
         name: "Mango People T-shirt #11",
-        price: 42.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_11.png",
+            price: 42.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 12,
-        img: "img/catalog_item_12.png",
         name: "Mango People T-shirt #12",
-        price: 62.34,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_12.png",
+            price: 62.34,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 13,
-        img: "img/catalog_item_13.png",
         name: "Mango People T-shirt #13",
-        price: 54.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_13.png",
+            price: 54.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 14,
-        img: "img/catalog_item_14.png",
         name: "Mango People T-shirt #14",
-        price: 53.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_14.png",
+            price: 53.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 15,
-        img: "img/catalog_item_15.png",
         name: "Mango People T-shirt #15",
-        price: 150,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_15.png",
+            price: 150,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 16,
-        img: "img/catalog_item_16.png",
         name: "Mango People T-shirt #16",
-        price: 120.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_16.png",
+            price: 120.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 17,
-        img: "img/catalog_item_17.png",
         name: "Mango People T-shirt #17",
-        price: 149.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_17.png",
+            price: 149.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 18,
-        img: "img/catalog_item_18.png",
         name: "Mango People T-shirt #18",
-        price: 32.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_18.png",
+            price: 32.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 19,
-        img: "img/catalog_item_19.png",
         name: "Mango People T-shirt #19",
-        price: 42.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_19.png",
+            price: 42.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 20,
-        img: "img/catalog_item_20.png",
         name: "Mango People T-shirt #20",
-        price: 62.34,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_20.png",
+            price: 62.34,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     },
     {
         id: 21,
-        img: "img/catalog_item_21.png",
         name: "Mango People T-shirt #21",
-        price: 54.00,
-        color: "red",
-        size: "XLL"
+        type: [{
+            img: "img/catalog_item_21.png",
+            price: 54.00,
+            star: 4.5,
+            color: "red",
+            size: "XLL"
+        }]
     }
 
 ];
@@ -193,12 +256,12 @@ class CPoductCatalogHTML {
         this.html.innerHTML = this.items.map(item => this.itemGen(this.catalog.find(obj => { return obj.id === item }))).join('');
     }
 
-    itemGen(_obj) {
-        return `<li id="catalogItem-${_obj.id}" class="products-grid_item">
+    itemGen(_obj,_t = 0) {
+        return `<li data-catalog-item="${_obj.id}" data-catalog-item-t="${_t}" class="products-grid_item">
                         <div class="top-block">
-                            <div class="visible"><img src="${_obj.img}" alt=""></div>
+                            <div class="visible"><img src="${_obj.type[_t].img}" alt=""></div>
                             <div class="hovered">
-                                <div class="btn-sqr_adcw"><a href="cart/itemAdd/${_obj.id}" onclick="cart.itemAdd(${_obj.id});return false;" class="btn-sqr_adcw__link clear-fix">
+                                <div class="btn-sqr_adcw"><a href="cart/addItem/${_obj.id}/${_t}" onclick="cart.addAct(this);return false;" class="btn-sqr_adcw__link clear-fix">
                                         <div class="btn-sqr_adcw__wrap"><img src="img/cart_white.png" alt="" class="btn-sqr_adcw__icon"><span class="btn-sqr_adcw__text">Add to Cart</span></div>
                                     </a>
                                 </div>
@@ -206,7 +269,7 @@ class CPoductCatalogHTML {
                         </div>
                         <a href="#" class="products-grid_link">
                             <h3>${_obj.name}</h3>
-                            <p>$${float2str(_obj.price)}</p>
+                            <p>$${float2str(_obj.type[_t].price)}</p>
                         </a>
                     </li>`;
     }
