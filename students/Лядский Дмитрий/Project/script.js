db340c112f8cb218dd022450529a8941e0133c9d
Smart97 <Smart9713@gmail.com>
2021-03-01 19:21:18 +0300

    2hw

--- students/Лядский Дмитрий/Project/script.js
+++ students/Лядский Дмитрий/Project/script.js
@@ -1,24 +1,92 @@
-const goods = [
-    { title: 'Монитор', price: 50000 },
-    { title: 'Клавиатура', price: 1500 },
-    { title: 'Мышь', price: 700 },
-    { title: 'Ноутбук', price: 35000 },
-    { title: 'Ноутбук', price: 35000 },
-    { title: 'Ноутбук', price: 35000 },
-];
+class GoodsItem {
+    constructor(item) {
+        this.item = item;
+    }
 
-const getGoodsLayout = (title, price) => {
-    return `
-        <div class="item">
-            <h2>${title}</h2>
-            <p>${price}</p>
-        </div>
-    `;
+    render() {
+        return `
+            <div class="item">
+                <h2>${this.item.title}</h2>
+                <p>${this.item.price}</p>
+            </div>
+        `;
+    }
 }
 
-const renderGoods = (list) => {
-    const goodsString = list.map(element => getGoodsLayout(element.title, element.price));
-    document.querySelector('.goods').innerHTML = goodsString.join('');
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchData() {
+        this.goods = [
+            { title: 'Монитор', price: 50000 },
+            { title: 'Клавиатура', price: 1500 },
+            { title: 'Мышь', price: 700 },
+            { title: 'Ноутбук', price: 35000 },
+        ];
+    }
+
+    render() {
+        const goodsString = this.goods.map(element => {
+            const item = new GoodsItem(element);
+            return item.render();
+        });
+        document.querySelector('.goods').innerHTML = goodsString.join('');
+    }
 }
 
-renderGoods(goods);
+
+class Basket {
+
+    constructor() {
+        this.basketItems = [
+            { title: 'Монитор', price: 50000, amount: 2 },
+            { title: 'Клавиатура', price: 1500, amount: 1 },
+            { title: 'Что-то', price: 5000, amount: 3 },
+        ];
+        this.totalCost = 0;
+    }
+    calcCost() {
+        this.totalCost = 0;
+        this.basketItems.forEach(element => this.totalCost += element.price*element.amount);
+    }
+
+    fetchData() {
+        // запрос данных с сервера
+    }
+
+    render() {
+        //
+    }
+
+    saveBasket() {
+        //сохраняет содержимое корзины на сервере
+    }
+
+    wipeBasket() {
+        //удаляет все содержимое корзины
+    }
+
+
+}
+
+
+class BasketItem {
+    deleteItem() {
+        //удаляет предмет из корзины
+    }
+    increase() {
+        //увеличивает кол-во предметов в корзине
+    }
+    decrease(){
+        //уменьшает
+    }
+}
+
+
+const list = new GoodsList();
+const basket1 = new Basket();
+basket1.calcCost()
+list.fetchData();
+list.render();
