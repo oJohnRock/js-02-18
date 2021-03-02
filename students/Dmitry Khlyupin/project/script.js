86950d6981f1e5ed6b8fb06c5ce3fcb379e28d4c
dmhlupin <45971646+dmhlupin@users.noreply.github.com>
2021-03-02 19:39:08 +0300

    Homework 2
    В каталоге project 1 и 2 части задания
    В каталоге other скрипт выполненного задания под звездочкой

--- students/Dmitry Khlyupin/project/script.js
+++ students/Dmitry Khlyupin/project/script.js
@@ -1,22 +1,116 @@
-const goods = [
-    { title: 'Монитор', price: 50000 },
-    { title: 'Клавиатура', price: 1500 },
-    { title: 'Мышь', price: 700 },
-    { title: 'Ноутбук', price: 35000 },
-];
-
-const getGoodsLayout = (title="GoodTitle", price="GoodPrice") =>  //упрощаем функцию, убирая return и скобки
-     `
-        <div class="item">
-            <h2>${title}</h2>
-            <p>${price}</p>
+class GoodsItem {
+    constructor (title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `
+        <div class="goods-item">
+            <h3>
+                ${this.title}
+            </h3>
+            <p>
+                ${this.price}
+            </p>
         </div>
-    `;
-
-
-const renderGoods = list => {  // одна переменная, можно без скобок
-    const goodsString = list.map(element => getGoodsLayout(element.title, element.price)).join('');// убираем разделитель
-    document.querySelector('.goods').innerHTML = goodsString;
+        `;
+    }
 }
 
-renderGoods(goods);
+class GoodsList {
+    constructor(){
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            {title: 'Shirt', price: 150},
+            {title: 'Socks', price: 50},
+            {title: 'Jacket', price: 350},
+            {title: 'Shoes', price: 250}
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title,good.price);
+            listHtml += goodItem.render();
+        });
+
+        //Добавим вывод стоимости всех товаров на страницу
+        listHtml += `                                                               
+            <p>Стоимость всех товаров равна: ${this.calcFullPrice()} рублей.</p>  
+        `;
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+
+    // метод возвращающий стоимость всех товаров
+    calcFullPrice() {   
+        return this.goods.reduce((acc, curr) => {return acc + curr.price}, 0);
+    }
+}
+
+class Cart {
+    constructor() {
+        this.goods = [];
+    }
+    // Опишем методы корзины:
+    renderCart() {  // - метод для отрисовки корзины
+
+    };     
+
+    addItemToCart() { // - метод для добавления элемента в корзину
+
+    };
+
+    removeItemFromCart() { // - метод удаления элемента из корзины
+
+    };
+
+    calcItems() {  // - метод для подсчета количества элементов в корзине и их стоимости 
+
+    };
+
+    clearCart() { // - метод для очистки корзины
+
+    };
+
+    cartToBuy() {  // - метод продолжения покупки
+
+    };
+
+}
+
+class CartItem {
+    constructor(title, price, quantity = 1, discount = 0) { // Я бы ввел еще ID элемента, так как названия могут совпадать, в прошлом курсе так и делал.
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+        this.discount = discount;
+    }
+
+    //  Опишем методы элемента корзины
+    changeItemQuantity() { // - Изменение количества данного элемента в корзине
+
+    };
+
+    changeItemChars() {  // - Изменение характеристик элемента (например, размера)
+
+    };
+
+    renderItem() {  // - отрисовка элемента
+
+    };
+
+    calcDiscount() { // - расчет скидки
+
+    };
+}
+
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
+
+
