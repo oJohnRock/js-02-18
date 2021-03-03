777a7a7b924c168257997d48e7f45a50294d085b
MagicGTS <magicgts@gmail.com>
2021-03-03 18:27:23 +0300

    Add promise quering

--- students/Лешкевич Андрей А/project/js/init.js
+++ students/Лешкевич Андрей А/project/js/init.js
@@ -1,12 +1,38 @@
 "use strict";
 
 function pageInit() {
-    cart = new CCart(prodCatalog, cart2Render);
-    if ('undefined' !== typeof prodCatalog2Render) {
-        catalog = new CPoductCatalogHTML(document.getElementById("catalog"), prodCatalog, prodCatalog2Render);
-    }
+    (async () => {
+    await httpGet(urlCatalog).then(
+        response => {
+            prodCatalog = response['catalog']
+        },
+        error => {
+            prodCatalog = []
+        }
+    ).then(
+        httpGet(urlCart).then(
+            response => {
+                cart = new CCart(prodCatalog, response['cart']);
+            },
+            error => {
+                cart = new CCart(prodCatalog, []);
+            }
+        )
+    ).then(
+        
+        httpGet(urlCatalogDisplayed).then(
+            response => {
+                catalog = new CPoductCatalogHTML(document.getElementById("catalog"), prodCatalog, response['displayed']); 
+            },
+            error => {
+                catalog = new CPoductCatalogHTML(document.getElementById("catalog"), prodCatalog, []);
+            }
+        )
+        
+    );
     let tmp = document.getElementById("slider");
     if (null !== tmp) {
         slider = new CSliderHTML(tmp, ["https://picsum.photos/1920/1080?random=1", "img/catalog_item_22_big.png", "https://picsum.photos/1920/1080?random=2"]);
     }
+})();
 }
