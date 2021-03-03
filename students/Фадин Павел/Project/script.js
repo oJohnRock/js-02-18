d63fb01381a640ac20ba50e0c38c7df21d5d17bc
Павел Фадин <pavelfadin@MacBook-Air-Pavel.local>
2021-03-03 22:05:34 +0300

    Hw2

--- students/Фадин Павел/Project/script.js
+++ students/Фадин Павел/Project/script.js
@@ -5,18 +5,58 @@ const goods = [
   { title: 'Ноутбук', price: 35000 },
 ];
 
-const getGoodsLayout = (title, price = 'Цену уточняйте у продавца') => `
+class GoodItem {
+  constructor(item) {
+    this.item = item;
+  }
+
+  render() {
+    return `
         <div class="item">
-            <h2>${title}</h2>
-            <p>${price}</p>
+            <h2>${this.item.title}</h2>
+            <p>${this.item.price}</p>
         </div>
     `;
+  }
+}
 
-const renderGoods = (list = []) => {
-  const goodsString = list.map((element) =>
-    getGoodsLayout(element.title, element.price)
-  );
-  document.querySelector('.goods').innerHTML = goodsString.join('');
-};
+class GoodList {
+  constructor(goodsArray) {
+    this.goods = goodsArray;
+  }
 
-renderGoods(goods);
+  render() {
+    const goodsString = this.goods.map((element) => {
+      const item = new GoodItem(element);
+      return item.render();
+    });
+    document.querySelector('.goods').innerHTML = goodsString.join('');
+  }
+}
+
+class Basket {
+  fetchData() {} // запрос данны с сервера на получение списка товара
+
+  addOnCart() {} // добавление в корзину
+
+  cartRemove() {} // удаление из корзины
+
+  cartClear() {} // очистка корзины
+
+  changeCartCount() {} // меняем количество товаров в корзине
+
+  cartRecalculate() {} // пересчет корзины
+
+  basketTotal() {
+    const sum = goods.reduce((total, current) => total + current.price, 0);
+    console.log(sum);
+  }
+
+  render() {} // отрисовка полученных товаров
+}
+
+class BasketItem {}
+
+const list = new GoodList(goods);
+
+list.render();
